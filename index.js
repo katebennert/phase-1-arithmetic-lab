370e660c05af30bc40e23d503a259f8e48637d21
Kate Bennert <kate.bennert@gmail.com>
2022-06-07 22:05:22 -0400

    commit tree 86d50c4fd0d121bb9fb711334b89cad609a5da65

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 15.5;
+const num2 = 4;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
